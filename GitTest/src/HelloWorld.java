317ade6a186f1cb1935ba9f43562610a6fb0c45b
techny <technykim@gmail.com>
2016-05-25 14:34:58 +0900

    master branch changes a code

--- GitTest/src/HelloWorld.java
+++ GitTest/src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 	public static void main(String[] args){
 		System.out.print("Hello World");
 		System.out.print("first branch test");
+		System.out.print("second branch test in Master");
 	}
 }
